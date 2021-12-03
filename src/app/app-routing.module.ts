16f318cb7aeb17ea719d01978437c0b2ec838ce7
Mowgli Sapa Mecha <mowglimecha@Mowglis-MacBook-Pro.local>
2021-12-03 12:28:07 +0800

    updated readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import {AccordionModule} from 'primeng/accordion';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    AccordionModule,
+
+    
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
